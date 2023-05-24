6bdeebe74833590a56ac6d9b9c05b4ebb08f5bc0
natalia-ya <sagaidak.nata@gmail.com>
2023-05-24 15:34:31 +0200

    Updated the conditional rendering for the sidebar

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 import "./App.css";
 
 const App = () => {
-  const activeMenu = true;
+  const activeMenu = false;
   return (
     <div>
       <BrowserRouter>
@@ -22,7 +22,13 @@ const App = () => {
               </button>
             </TooltipComponent>
           </div>
-          {activeMenu ? <div>Sidebar</div> : <div>Sidebar w-0</div>}
+          {activeMenu ? (
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+              Sidebar
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">Sidebar</div>
+          )}
         </div>
       </BrowserRouter>
     </div>
