b9acab7c45d11e829b503309710cb9ede79351e2
natalia-ya <sagaidak.nata@gmail.com>
2023-05-28 05:28:36 +0200

    Imported necessary components and pages for the app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,26 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { FiSettings } from "react-icons/fi";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
+import { Navbar, Footer, Sidebar, ThemeSettings } from "./components";
+import {
+  Ecommerce,
+  Orders,
+  Employees,
+  Customers,
+  Kanban,
+  Editor,
+  Calendar,
+  ColorPicker,
+  Line,
+  Area,
+  Bar,
+  Pie,
+  Financial,
+  ColorMapping,
+  Pyramid,
+  Stacked,
+} from "./pages";
+
 import "./App.css";
 
 const App = () => {
