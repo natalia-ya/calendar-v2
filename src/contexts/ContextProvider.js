757b19b64a831310dcfc2291e9ef10f0b1422175
natalia-ya <sagaidak.nata@gmail.com>
2023-06-02 14:18:51 +0200

    Modified the ContextProvider component to include the state variable activeMenu using the useState hook and added the setActiveMenu function to update its value

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -10,9 +10,16 @@ const initialState = {
 };
 
 export const ContextProvider = ({ children }) => {
-    return (
-        <StateContext.Provider
-            value={{ test: 'test' }}>
-            {children}
-            </StateContext.Provider>
-    )
+  const [activeMenu, setActiveMenu] = useState(true);
+  return (
+    <StateContext.Provider
+      value={{
+        activeMenu,
+      }}
+    >
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
