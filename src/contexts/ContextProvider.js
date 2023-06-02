27a5cbce607a568ae9004944f65867fe3fca4ab6
natalia-ya <sagaidak.nata@gmail.com>
2023-06-02 14:13:08 +0200

    Exported ContextProvider component

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -8,3 +8,11 @@ const initialState = {
   userProfile: false,
   notification: false,
 };
+
+export const ContextProvider = ({ children }) => {
+    return (
+        <StateContext.Provider
+            value={{ test: 'test' }}>
+            {children}
+            </StateContext.Provider>
+    )
