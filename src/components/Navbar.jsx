b03b97c61311de4fdba0587cbd03e173107eab4d
natalia-ya <sagaidak.nata@gmail.com>
2023-06-03 12:17:30 +0200

    Enhanced Navbar component with a customizable NavButton.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,8 +10,36 @@ import avatar from "../data/avatar.jpg";
 import { Cart, Chat, Notification, UseProfile } from ".";
 import { useStateContext } from "../contexts/ContextProvider";
 
+const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
+  <TooltipComponent content={title} position="ButtomCenter">
+    <button
+      type="button"
+      onClick={customFunc}
+      style={{ color }}
+      className="relative text-x1 rounded-full p-3 hover:bg-light-gray"
+    >
+      <span
+        style={{ background: dotColor }}
+        className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
+      >
+        {icon}
+      </span>
+    </button>
+  </TooltipComponent>
+);
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const { activeMenu, setActiveMenu } = useStateContext();
+  return (
+    <div className="flex justify-between p-2 md:mx-6 relative">
+      <NavButton
+        title="Menu"
+        customFunc={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)}
+        color="blue"
+        icon={<AiOutlineMenu />}
+      />
+    </div>
+  );
 };
 
 export default Navbar;
