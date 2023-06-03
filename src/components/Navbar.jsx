42292568535edb80e600b4071ae0984a86f1b59a
natalia-ya <sagaidak.nata@gmail.com>
2023-06-03 13:38:13 +0200

    Added additional NavButtons for Cart, Chat, and Notifications, along with a profile dropdown in the Navbar component.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
-import { FiShoppingCard } from "react-icons/fi";
+import { FiShoppingCart } from "react-icons/fi";
 import { BsChatLeft } from "react-icons/bs";
 import { RiNotification3Line } from "react-icons/ri";
 import { MdKeyboardArrowDown } from "react-icons/md";
@@ -38,6 +38,44 @@ const Navbar = () => {
         color="blue"
         icon={<AiOutlineMenu />}
       />
+      <div className="flex">
+        <NavButton
+          title="Cart"
+          customFunc={() => handleClick("cart")}
+          color="blue"
+          icon={<FiShoppingCart />}
+        />
+        <NavButton
+          title="Chat"
+          dotColor="#03C9D7"
+          customFunc={() => handleClick("chat")}
+          color="blue"
+          icon={<BsChatLeft />}
+        />
+        <NavButton
+          title="Notifications"
+          dotColor="#03C9D7"
+          customFunc={() => handleClick("notofication")}
+          color="blue"
+          icon={<RiNotification3Line />}
+        />
+        <TooltipComponent content="Profile" position="BottomCenter">
+          <div
+            className="flex item-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg"
+            onClick={() => handClick("userProfile")}
+          >
+            <img className="rounded-full w-8 h-8" src={avatar} />
+            <p>
+              <span className="text-gray-400 text-14">Hi,</span>
+              {""}
+              <span className="text-gray-400 font-bold ml-1 text-14 ">
+                Michael
+              </span>
+            </p>
+            <MdKeyboardArrowDown className="text-gray-400 text-14" />
+          </div>
+        </TooltipComponent>
+      </div>
     </div>
   );
 };
