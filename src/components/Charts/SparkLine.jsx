bddbb9e87ca3b7be5a73875eef7e17a18389f834
natalia-ya <sagaidak.nata@gmail.com>
2023-06-04 20:05:48 +0200

    Adds the SparkLine component to the project

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import {
+  SparklineComponent,
+  Inject,
+  SparklineTooltip,
+} from "@syncfusion/ej2-react-charts";
 
 const SparkLine = () => {
   return <div>SparkLine</div>;
