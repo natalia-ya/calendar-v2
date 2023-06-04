2953ead53cd237ea66884187814528ad4df01c94
natalia-ya <sagaidak.nata@gmail.com>
2023-06-04 22:37:55 +0200

    Refactor the Orders component

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Resize,
+  Sort,
+  ContextMenu,
+  Filter,
+  Page,
+  ExcelExport,
+  PdfExport,
+  Edit,
+  Inject,
+} from "@syncfusion/ej2-react-grids";
+
+import { orderDetails, contextMenuItems, orderGrid } from "../../data/dummy";
+import { Header } from "../components";
 
 const Orders = () => {
-  return <div>Orders</div>;
+  return (
+    <div>
+      <Header category="Page" title="Orders" />
+    </div>
+  );
 };
 
 export default Orders;
